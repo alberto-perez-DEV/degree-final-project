5555ab2435d36c40a9e065cac02c8251bf0ab4f7
Alberto Pérez <56350482+C0nf1cker@users.noreply.github.com>
2023-07-27 15:34:59 +0200

    WIP mongoDB connection script

---
+++ backend/src/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// -- DB CONNECTION --
+
+const URI = process.env.MONGODB_URI
+                ? process.env.MONGODB_URI
+                : "mongodb://localhost/default_db";
+
+mongoose.connect(URI);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB connected:', URI);
+});
