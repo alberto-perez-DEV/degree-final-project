720cffa12a1aff80e286d2f7a89092a7f7cb8bd8
cfkr-dev <albertoperezperez1@hotmail.com>
2023-07-28 01:22:36 +0200

    Last configs

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,5 +9,12 @@ app.set('port', process.env.PORT || 4000);
 
 // -- MIDDLEWARE --
 app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// -- ROUTES --
+app.get(('/'), (req, res) => {
+    res.send('Welcome!');
+})
 
 module.exports = app;
