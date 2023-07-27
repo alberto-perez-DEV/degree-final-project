e15d0a6bb7d2ab1739cb30a9ed9654e46b11b0fa
Alberto Pérez <56350482+C0nf1cker@users.noreply.github.com>
2023-07-27 15:01:03 +0200

    basic script to run app

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+// -- EXECUTE BACKEND SERVER --
+
+async function runApp() {
+    await app.listen(app.get('port'))
+    console.log('Server is running on port', app.get('port'));
+}
+
+runApp();
